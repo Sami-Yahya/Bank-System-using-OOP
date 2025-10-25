6068fb9573235385f5d49c5ba88b126bd6d2c4a6
Sami Yahya <p.sami.yahya@gmail.com>
2025-10-25 14:52:38 +0300

    Create Login Screen after Logout.cpp

---
+++ Stage 10.2 [Login & Permissions (Show Login Screen after Logout)]/Login Screen after Logout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+using namespace std;
+
+int main() {
+
+    while (true)
+    {
+        clsLoginScreen::ShowLoginScreen();
+    }
+
+
+   // system("pause > 0");
+    return 0;
+}
