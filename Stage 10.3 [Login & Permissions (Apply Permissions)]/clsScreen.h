ca9f3494c1cd31bb4d1d7abdecc8bcbd2a8073c6
Sami Yahya <p.sami.yahya@gmail.com>
2025-10-26 14:20:05 +0300

    Add files via upload

---
+++ Stage 10.3 [Login & Permissions (Apply Permissions)]/clsScreen.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include "clsUser.h"
+#include "Global.h"
+using namespace std;
+
+class clsScreen
+{
+protected:
+
+	static void _DrawScreenHeader(string Title, string SubTitle = "")
+	{
+        cout << "\t\t\t\t\t______________________________________";
+        //cout << "\t\t\t\t\t--------------------------------------";
+        cout << "\n\n\t\t\t\t\t  " << Title;
+
+        if (SubTitle != "")
+        {
+            cout << "\n\t\t\t\t\t  " << SubTitle;
+        }
+
+        cout << "\n\t\t\t\t\t______________________________________\n\n";
+        //cout << "\n\t\t\t\t\t--------------------------------------\n\n";
+	}
+
+    static bool _CheckAccessRights(clsUser::enUserPermissions Permission)
+    {
+        if (!CurrentUser.CheckAccessPermission(Permission))
+        {
+            cout << "\t\t\t\t\t--------------------------------------\n";
+            cout << "\t\t\t\t\t\t   Access Denied,\n";
+            cout << "\t\t\t\t\tYou don't have Permission to do this,\n";
+            cout << "\t\t\t\t\t     Please contact your Admin.\n";
+            cout << "\t\t\t\t\t--------------------------------------\n";
+            
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+};
